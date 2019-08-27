e88cff209ca78d21fc88750c758be889fb650aee
cbr9 <cabero96@hotmail.com>
2019-08-27 11:45:39 +0200

    big refactor

--- organizer.py
+++ organizer.py
@@ -13,32 +13,28 @@ class Handler(FileSystemEventHandler):
 		self.__HOME = os.environ["HOME"]
 		self.pdf_dst = os.path.join(self.__HOME, "Documents")
 		self.img_dst = os.path.join(self.__HOME, "Pictures")
-		self.tracked_folder = os.path.join(self.__HOME, "Downloads")
-		os.chdir(self.tracked_folder)
+		self.tracked = os.path.join(self.__HOME, "Downloads")
+		os.chdir(self.tracked)
 
 	def on_modified(self, event):
-		global src, file
-		for root, dirs, files in os.walk(os.getcwd(), topdown=True):
-			for file in files:
-				_, extension = os.path.splitext(file)
-				src = os.path.join(root, file)
-				if extension == ".pdf":
-					self.move_file(dst=self.pdf_dst)
-				elif imghdr.what(src):
-					if src.endswith(".part"):
-						time.sleep(10)
-						src = src.strip("part").strip(".")
-					self.move_file(dst=self.img_dst)
+		if os.path.isfile(event.src_path):
+			_, extension = os.path.splitext(event.src_path)
+			if extension == ".pdf":
+				self.move_file(src=event.src_path, dst=self.pdf_dst)
+			elif imghdr.what(event.src_path):
+				if not event.src_path.endswith(".part"):
+					self.move_file(src=event.src_path, dst=self.img_dst)
 
 	@staticmethod
-	def move_file(dst):
+	def move_file(src, dst):
+		file = os.path.basename(src)
 		dst = os.path.join(dst, file)
-		shutil.move(src, dst=dst)
+		shutil.move(src=src, dst=dst)
 
 
 event_handler = Handler()
 observer = Observer()
-observer.schedule(event_handler, event_handler.tracked_folder, recursive=True)
+observer.schedule(event_handler, event_handler.tracked, recursive=True)
 observer.start()
 
 try:
@@ -47,3 +43,5 @@ try:
 except KeyboardInterrupt:
 	observer.stop()
 	observer.join()
+
+
